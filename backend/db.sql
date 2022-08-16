06bdf21200064284d46e654341cd5a5ddd83d98b
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2022-08-16 03:12:23 +0200

    Create db.sql

---
+++ backend/db.sql
@@ -0,0 +1,34 @@
+/* psql -h localhost -d fitness -U postgres -f db.sql */
+
+DROP TABLE IF EXISTS foods CASCADE;
+DROP TABLE IF EXISTS menus CASCADE;
+DROP TABLE IF EXISTS menu_foods CASCADE;
+
+CREATE TABLE IF NOT EXISTS foods (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  market TEXT NOT NULL,
+  calories FLOAT NOT NULL,
+  carbs FLOAT NOT NULL,
+  proteins FLOAT NOT NULL,
+  fats FLOAT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS menus (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  number_of_meals INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS menu_foods (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  menu_id INT NOT NULL,
+  food_id INT NOT NULL,
+  quantity INT NOT NULL,
+  meal_number INT NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_foods FOREIGN KEY (food_id) REFERENCES foods (id) ON DELETE CASCADE,
+  CONSTRAINT fk_menus FOREIGN KEY (menu_id) REFERENCES menus (id) ON DELETE CASCADE
+);
