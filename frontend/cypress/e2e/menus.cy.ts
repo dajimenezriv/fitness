87f6ee96a96dad0e468c75fe8bced17ea5ace720
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2022-09-29 19:21:57 +0200

    test menu foods

--- frontend/cypress/e2e/menus.cy.ts
+++ frontend/cypress/e2e/menus.cy.ts
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 
 /* eslint object-curly-newline: off */
+/* eslint no-restricted-syntax: off */
+/* eslint no-await-in-loop: off */
 
 export {};
 
@@ -10,6 +12,12 @@ const apiUrl = 'http://localhost:3001/api';
 const menus = [
   { name: 'Déficit', numberOfMeals: 3 },
   { name: 'Volumen', numberOfMeals: 5 },
+];
+
+const foods = [
+  { name: 'Banana', nutrients: { calories: 92, carbs: 23, proteins: 12, fats: 0.5 } },
+  { name: 'Arroz', nutrients: { calories: 350, carbs: 60, proteins: 6, fats: 0.001 } },
+  { name: 'Macarrones', nutrients: { calories: 186, carbs: 68, proteins: 7, fats: 0.0002 } },
 ];
 
 describe('Menus', () => {
@@ -28,6 +36,7 @@ describe('Menus', () => {
       menus.forEach((menu) => {
         cy.request('POST', `${apiUrl}/menus`, menu);
       });
+
       cy.visit(baseUrl);
     });
 
@@ -43,11 +52,52 @@ describe('Menus', () => {
       cy.get('html').should('not.contain', 'Déficit');
     });
 
-    it.only('Enter Menu Details', () => {
+    it('Enter Menu Details', () => {
       cy.contains('Déficit').click();
-      cy.contains('Cómida 1');
-      cy.contains('Cómida 2');
-      cy.contains('Cómida 3');
+      cy.contains('Comida 1');
+      cy.contains('Comida 2');
+      cy.contains('Comida 3');
+      cy.get('html').should('not.contain', 'Comida 4');
+    });
+
+    describe('Test menu foods', () => {
+      beforeEach(() => {
+        foods.forEach((food) => {
+          cy.request('POST', `${apiUrl}/foods`, food);
+        });
+        cy.contains('Déficit').click();
+
+        // variables
+        cy.get('[data-cy="add_menu_food"]').as('AddMenuFood');
+      });
+
+      it.only('All', () => {
+        // add banana 200g to meal 1
+        cy.get('@AddMenuFood').eq(0).click();
+        cy.contains(foods[0].name).click();
+        cy.get('[data-cy="quantity"]').type('200');
+        cy.get('[data-cy="save_menu_food"]').click();
+        cy.contains(Math.trunc(foods[0].nutrients.calories * 2));
+
+        cy.get('[data-cy="dialog_menu_food"]').type('{esc}');
+
+        // add banana 80g to meal 2
+        cy.get('@AddMenuFood').eq(1).click();
+        cy.get('[data-cy="dialog_menu_food"]').contains(foods[0].name).click();
+        cy.get('[data-cy="quantity"]').type('80');
+        cy.get('[data-cy="save_menu_food"]').click();
+        cy.contains(Math.trunc(foods[0].nutrients.calories * 0.8));
+
+        // add arroz 120g to meal 2
+        cy.get('[data-cy="dialog_menu_food"]').contains(foods[1].name).click();
+        cy.get('[data-cy="quantity"]').type('120');
+        cy.get('[data-cy="save_menu_food"]').click();
+        cy.contains(Math.trunc(foods[1].nutrients.calories * 1.2));
+
+        cy.get('[data-cy="dialog_menu_food"]').type('{esc}');
+
+        cy.contains('677.60'); // total
+      });
     });
   });
 });
